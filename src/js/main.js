7f5398b3b7ea56505dfe34b33f59a4f3a6d8a560
Julia Schantz <48764206+JuliaSchantz@users.noreply.github.com>
2020-10-08 22:43:07 -0400

    Develop (#1)
    * Create .DS_Store
    
    * Add image
    
    * Update index.html
    
    * Update CSS on home page
    
    * Add CSS file
    
    * Add vars file
    
    * Add grid photos
    
    * Formatting image grid HTML
    
    * Grid photo sizing
    
    * Adding large photo to grid
    
    * Add js file
    
    * Editting js file
    
    * Finish js for page loader
    
    * Logo animation
    
    * Reconfiguring for SASS
    
    * Changing variables to SASS
    
    * Update Labels
    
    * Update spacing
    
    Co-authored-by: Jjulze <48764206+Jjulze@users.noreply.github.com>

---
+++ src/js/main.js
@@ -0,0 +1,24 @@
+const loader = document.querySelector('.loader');
+const header = document.querySelector('header');
+const main = document.querySelector('main');
+const brand = document.querySelector('.brand__image');
+
+
+function init() {
+  setTimeout(() => {
+    loader.style.opacity = 0;
+    loader.style.display = 'none'
+
+    header.style.display = 'block';
+    setTimeout(() => (header.style.opacity) = 1, 50);
+
+    main.style.display = 'block';
+    setTimeout(() => (main.style.opacity) = 1, 50);
+
+    brand.style.display = 'block';
+    setTimeout(() => (brand.style.opacity) = 1, 500);
+  }, 3000);
+
+}
+
+init();
